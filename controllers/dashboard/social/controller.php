7f5c4ba5d2499fdadb53d78cb700317aa5b182f7
Sandro Padin <sandropadin@gmail.com>
2012-01-29 21:24:10 -0600

    Adding pretty buttons to dashboard page.

--- controllers/dashboard/social/controller.php
+++ controllers/dashboard/social/controller.php
@@ -5,7 +5,9 @@ Loader::model('user_list');
 class DashboardSocialController extends Controller { 
   
   public function view() {
-    
+    $html = Loader::helper('html');
+    $this->addHeaderItem($html->css('zocial/css/zocial.css', 'social'));
+    $this->addHeaderItem($html->css('master.css', 'social'));
   }
   
   public function on_before_render() {
--- single_pages/dashboard/social/view.php
+++ single_pages/dashboard/social/view.php
@@ -18,9 +18,8 @@ $flash = Loader::helper('flash_data','flash_data');
 </h1>
 <div class="ccm-dashboard-inner">
   <p>
-    <a href="/dashboard/social/facebook">Configure Facebook</a>
-  </p>
-  <p>
-    <a href="/dashboard/social/linkedin">Configure LinkedIn</a>
+    <a href="/dashboard/social/facebook" class="zocial facebook"><span>Configure Facebook</span></a>
+    <a href="/dashboard/social/linkedin" class="zocial linkedin"><span>Configure LinkedIn</span></a>
+    <a href="/dashboard/social/twitter" class="zocial twitter"><span>Configure Twitter</span></a>
   </p>
 </div>
